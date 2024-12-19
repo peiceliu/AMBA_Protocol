0f3ef1c0f2f0f20c617eeff36532788771de61b4
zhongkuncen <kuncenzhong@foxmail.com>
2024-12-19 16:51:53 +0800

    test1

--- AHB2APB_sync_Bridge/Design/interface.v
+++ AHB2APB_sync_Bridge/Design/interface.v
@@ -3,7 +3,7 @@ module interface #(
     parameter DATAWIDTH = 32,
 ) (
     // AHB bus signals
-    input                   HCLK, //
+    input                   HCLK, // AHB clock
     input                   HRESETn,
 
     input                   HSEL,
